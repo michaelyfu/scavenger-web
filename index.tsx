3404e007d9517aa0804bf4a81fa62ed90082e61b
michaelyfu <m888fu@gmail.com>
2023-01-29 00:21:15 -0500

    comment index.tsx

--- index.tsx
+++ index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Start/> */}
-    <App></App>
+    <Start/>
+    {/* <App></App> */}
   </React.StrictMode>
 );
